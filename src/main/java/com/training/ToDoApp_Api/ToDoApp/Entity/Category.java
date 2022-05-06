ae8dfbe8fc67fb12654fe00df3475b60349ab81a
SantiagoGM19 <santigm1905@gmail.com>
2022-05-06 12:04:55 -0500

    implementing entities

--- src/main/java/com/training/ToDoApp_Api/ToDoApp/Entity/Category.java
+++ src/main/java/com/training/ToDoApp_Api/ToDoApp/Entity/Category.java
@@ -1,4 +1,32 @@
 package com.training.ToDoApp_Api.ToDoApp.Entity;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Category")
+@Table(name = "categories")
+@Data
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Task> tasks = new ArrayList<>();
+
+    public Category addTask(Task task){
+        this.tasks.add(task);
+        return this;
+    }
+
 }
--- src/main/java/com/training/ToDoApp_Api/ToDoApp/Entity/Task.java
+++ src/main/java/com/training/ToDoApp_Api/ToDoApp/Entity/Task.java
@@ -1,4 +1,22 @@
 package com.training.ToDoApp_Api.ToDoApp.Entity;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "Task")
+@Table(name = "tasks")
+@Data
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private Boolean done;
+
+    private Long fkCategory;
 }
