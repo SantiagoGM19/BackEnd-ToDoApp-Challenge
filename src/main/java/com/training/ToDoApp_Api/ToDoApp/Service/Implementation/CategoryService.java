e9a72598c28abd681cd84b480e419cf18bad4f4e
SantiagoGM19 <santigm1905@gmail.com>
2022-05-06 12:18:39 -0500

    implementing services

--- src/main/java/com/training/ToDoApp_Api/ToDoApp/Service/Implementation/CategoryService.java
+++ src/main/java/com/training/ToDoApp_Api/ToDoApp/Service/Implementation/CategoryService.java
@@ -1,4 +1,34 @@
 package com.training.ToDoApp_Api.ToDoApp.Service.Implementation;
 
-public class CategoryService {
+import com.training.ToDoApp_Api.ToDoApp.Entity.Category;
+import com.training.ToDoApp_Api.ToDoApp.Repository.CategoryRepository;
+import com.training.ToDoApp_Api.ToDoApp.Service.Interfaces.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CategoryService implements ICategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
--- src/main/java/com/training/ToDoApp_Api/ToDoApp/Service/Implementation/TaskService.java
+++ src/main/java/com/training/ToDoApp_Api/ToDoApp/Service/Implementation/TaskService.java
@@ -1,4 +1,34 @@
 package com.training.ToDoApp_Api.ToDoApp.Service.Implementation;
 
-public class TaskService {
+import com.training.ToDoApp_Api.ToDoApp.Entity.Task;
+import com.training.ToDoApp_Api.ToDoApp.Repository.TaskRepository;
+import com.training.ToDoApp_Api.ToDoApp.Service.Interfaces.ITaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class TaskService implements ITaskService {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Override
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
